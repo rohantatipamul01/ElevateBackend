ce2441225192fa2ce8bd1c55dfad3952748f0a96
Rohan <rohantatipamul01@gmail.com>
2026-07-22 18:35:16 +0530

    Remove duplicate PasswordEncoder bean

--- src/main/java/com/elevate/ElevateBackend/config/SecurityConfig.java
+++ src/main/java/com/elevate/ElevateBackend/config/SecurityConfig.java
@@ -10,8 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -43,10 +42,7 @@ public class SecurityConfig {
         this.successHandler = successHandler;
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    
 
     @Bean
     public AuthenticationManager authenticationManager(
